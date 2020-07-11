8bcf0e0bb37ff8ac60cafac3d9765a5d9028d046
Troy Kuhn <troykuhn22@gmail.com>
2020-07-11 16:08:13 -0400

    Practice stuff and Classes

--- 9-mediumConcepts/3-classes.js
+++ 9-mediumConcepts/3-classes.js
@@ -12,7 +12,7 @@ class Person {
     }
 
     greeting () {
-
+        return `Hello, my name is ${this.name}`;
     }
 }
 
--- Challenges/Practice.js
+++ Challenges/Practice.js
@@ -64,33 +64,75 @@
 
 //      console.log(favoriteMovie.characters.charOne.items.itemOne);
 
-let favoriteMovie = {
-    nameOfMovie: "Interstellar",
-    runTime: "2 Hours",
-    genre: 'Science fiction',
-    characters:
-     {
+// let favoriteMovie = {
+//     nameOfMovie: "Interstellar",
+//     runTime: "2 Hours",
+//     genre: 'Science fiction',
+//     characters:
+//      {
+//       charOne: {
+//         name: "McConaughey",
+//         age: 45,
+//         items: [
+//           { itemOne: "Spaceship" },
+//           { itemTwo: "Helmet" }, 
+//       ]
+//       },
+//       charTwo: {
+//         name: "Murph",
+//         age: 35,
+//         items: [
+//           { itemOne: "Red Hair" },
+//           { itemTwo: "Love For Her Father"}
+//       ]
+//       }
+//     },
+//   };
+  
+//   console.log(favoriteMovie.nameOfMovie);
+//   console.log(favoriteMovie.runTime);
+//   console.log(favoriteMovie.characters)
+//   console.log(favoriteMovie.characters.charTwo.name)
+//   console.log(favoriteMovie.characters.charTwo.items.itemTwo);
+
+let favMovie = {
+  nameOfMovie: 'Interstellar',
+  runTime: 200,
+  genre: 'Science Fiction',
+  characters: [
+    {
       charOne: {
-        name: "McConaughey",
-        age: 45,
+        name: 'Murph',
+        age: 30,
         items: [
-          { itemOne: "Spaceship" },
-          { itemTwo: "Helmet" }, 
-      ]
-      },
-      charTwo: {
-        name: "Murph",
-        age: 35,
-        items: [
-          { itemOne: "Red Hair" },
-          { itemTwo: "Love For Her Father"}
-      ]
+          {itemOne: 'Red Hair',
+          itemTwo: 'Ghost in her Room'}
+        ]
       }
     },
-  };
-  
-  console.log(favoriteMovie.nameOfMovie);
-  console.log(favoriteMovie.runTime);
-  console.log(favoriteMovie.characters)
-  console.log(favoriteMovie.characters.charTwo.name)
-  console.log(favoriteMovie.characters.charTwo.items.itemTwo);
+      {
+        charTwo: {
+          name: 'Cooper',
+          age: 40,
+          items: [
+            {itemOne: 'Spacesuit',
+            itemTwo: 'Love for his daughter'}
+          ]
+        }
+      }
+    ]
+  }
+
+  function getCharacterByName(name) {
+    for (let character in favMovie.characters){
+      if(name === character.value);
+      console.log(name);
+    }
+  }
+
+console.log(favMovie.nameOfMovie);
+console.log(favMovie.runTime);
+console.log(favMovie.characters)
+console.log(favMovie.characters[1].charTwo.name);
+console.log(favMovie.characters[1].charTwo.items[0].itemOne);
+// console.log(favMovie.characters.charTwo.items.itemOne);
